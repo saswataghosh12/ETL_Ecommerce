cf7e4855eff7e28d88f93a431a7656a3258c5cf0
saswataghosh12 <67067057+saswataghosh12@users.noreply.github.com>
2024-01-31 00:32:23 +0530

    Update Flipkart_dag.py
    Updated Logic for lvl2 ingestion

--- Flipkart_dag.py
+++ Flipkart_dag.py
@@ -1,8 +1,7 @@
 import datetime
 
 import airflow
-from airflow.operators import bash_operator
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
 from airflow import models
 from airflow.operators.dummy_operator import DummyOperator
@@ -10,6 +9,8 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.google.cloud.operators.dataflow import DataflowCreatePythonJobOperator
 
 file_name="flipkart_mobiles.csv"
+#variables
+gcp_sql_path ="gs://landing12/sql_script"
 
 YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
@@ -58,11 +59,11 @@ with models.DAG(
 
     )
 
-    # insert_mobile_price_into_trst_table = BigQueryOperator(
-    #     task_id='insert_mobile_price_into_trst_table',
-    #     sql='/SQL/insert_mobile_price_trst.sql',
-    #     use_legacy_sql=False,
-    # )
+    insert_mobile_price_into_trst_table = BashOperator(
+        task_id='insert_mobile_price_into_trst_table',
+        bash_command= 'bq query --nouse_legacy_sql "`gsutil cat {}/Insert_data_dml.sql`"'.foramt(gcp_sql_path),
+        dag=dag,
+    )
 
     archive_file = GCSToGCSOperator(
         task_id="archive_file",
